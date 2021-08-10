b9441cab9ff0f7829735d140b0689f9bc1bee21a
samroodAli <devsamrood@gmail.com>
2021-08-10 16:00:48 +0530

    Refactor:Refactor background image and mountains to seperate super class

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,62 +1,14 @@
-import Phaser from "phaser";
-import { gameConfig, gameOptions } from "../gameOptions.js";
+import Background from "./background.js";
 
-class Game extends Phaser.Scene {
+class Game extends Background {
   constructor() {
     super("Game");
   }
-  setBackground() {
-    this.background = this.add
-      .image(gameConfig.width / 2, gameConfig.height / 2, "blueLand")
-      .setScrollFactor(1, 0);
-    this.background.displayWidth = gameConfig.width;
-  }
-  getLastMountainX() {
-    let lastMountainX = -200;
-    this.mountainGroup.getChildren().forEach((mountain) => {
-      lastMountainX = Math.max(lastMountainX, mountain.x);
-    });
-    return lastMountainX;
-  }
-  addMountains() {
-    const lastMountainX = this.getLastMountainX();
-    if (lastMountainX < gameConfig.width * 2) {
-      let mountain = this.physics.add.sprite(
-        lastMountainX + Phaser.Math.Between(100, 350),
-        gameConfig.height - Phaser.Math.Between(0, 150),
-        "mountain"
-      );
-      mountain.setOrigin(0.5, 1);
-      mountain.body.setVelocityX(gameOptions.mountainSpeed * -1);
-      this.mountainGroup.add(mountain);
-      if (Phaser.Math.Between(0, 1)) {
-        mountain.setDepth(1);
-      }
-      mountain.setFrame(Phaser.Math.Between(0, 3));
-      this.addMountains();
-    }
-  }
-  recycleMountains() {
-    this.mountainGroup.getChildren().forEach((mountain) => {
-      if (mountain.x < -mountain.displayWidth) {
-        let lastMountainX = this.getLastMountainX();
-        mountain.x = lastMountainX + Phaser.Math.Between(100, 350);
-        mountain.y = gameConfig.height + Phaser.Math.Between(0, 100);
-        mountain.setFrame(Phaser.Math.Between(0, 3));
-        if (Phaser.Math.Between(0, 1)) {
-          mountain.setDepth(1);
-        }
-      }
-    });
-  }
-  preload() {}
   create() {
-    this.setBackground();
-    this.mountainGroup = this.add.group();
-    this.addMountains();
+    super.create();
   }
   update() {
-    this.recycleMountains();
+    super.update();
   }
 }
 export default Game;
