cdf5a8cbafeddf8c35897dcec3bb2f6f491f920a
samroodAli <devsamrood@gmail.com>
2021-08-10 15:46:23 +0530

    Fix:Fix mountain multiple ones added to mountainGroup

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -5,6 +5,12 @@ class Game extends Phaser.Scene {
   constructor() {
     super("Game");
   }
+  setBackground() {
+    this.background = this.add
+      .image(gameConfig.width / 2, gameConfig.height / 2, "blueLand")
+      .setScrollFactor(1, 0);
+    this.background.displayWidth = gameConfig.width;
+  }
   getLastMountainX() {
     let lastMountainX = -200;
     this.mountainGroup.getChildren().forEach((mountain) => {
@@ -17,12 +23,12 @@ class Game extends Phaser.Scene {
     if (lastMountainX < gameConfig.width * 2) {
       let mountain = this.physics.add.sprite(
         lastMountainX + Phaser.Math.Between(100, 350),
-        gameConfig.height + Phaser.Math.Between(0, 100),
+        gameConfig.height - Phaser.Math.Between(0, 150),
         "mountain"
       );
       mountain.setOrigin(0.5, 1);
       mountain.body.setVelocityX(gameOptions.mountainSpeed * -1);
-      this.mountainGroup.addMultiple(mountain);
+      this.mountainGroup.add(mountain);
       if (Phaser.Math.Between(0, 1)) {
         mountain.setDepth(1);
       }
@@ -30,18 +36,27 @@ class Game extends Phaser.Scene {
       this.addMountains();
     }
   }
+  recycleMountains() {
+    this.mountainGroup.getChildren().forEach((mountain) => {
+      if (mountain.x < -mountain.displayWidth) {
+        let lastMountainX = this.getLastMountainX();
+        mountain.x = lastMountainX + Phaser.Math.Between(100, 350);
+        mountain.y = gameConfig.height + Phaser.Math.Between(0, 100);
+        mountain.setFrame(Phaser.Math.Between(0, 3));
+        if (Phaser.Math.Between(0, 1)) {
+          mountain.setDepth(1);
+        }
+      }
+    });
+  }
   preload() {}
   create() {
     this.setBackground();
     this.mountainGroup = this.add.group();
     this.addMountains();
   }
-
-  setBackground() {
-    this.background = this.add
-      .image(gameConfig.width / 2, gameConfig.height / 2, "blueLand")
-      .setScrollFactor(1, 0);
-    this.background.displayWidth = gameConfig.width;
+  update() {
+    this.recycleMountains();
   }
 }
 export default Game;
