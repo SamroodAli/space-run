996d1e5fe858b51969fbb720698e4379bafc53ab
samroodAli <devsamrood@gmail.com>
2021-08-10 17:04:30 +0530

    Player:Create player and set scale and gravity

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,7 +1,19 @@
 import Platform from "./platform.js";
+import { gameConfig, gameOptions } from "../gameOptions.js";
 class Game extends Platform {
   constructor() {
     super("Game");
+  }
+  create() {
+    super.create();
+    this.player = this.physics.add.sprite(
+      gameOptions.playerStartPosition,
+      gameConfig.height / 2,
+      "player"
+    );
+    this.player.setScale(0.5);
+    this.player.setDepth(2);
+    this.player.setGravityY(gameOptions.playerGravity);
   }
 }
 
