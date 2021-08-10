5cec837cc0921c374b89deef900f79e45cba2ebe
samroodAli <devsamrood@gmail.com>
2021-08-10 11:44:58 +0530

    GameOptions:Create and export gameOptions object

---
+++ src/gameOptions.js
@@ -0,0 +1,14 @@
+export const gameOptions = {
+  platformSpaceRange: [300, 400],
+  mountainSpeed: 80,
+  spawnRange: [80, 300],
+  platformSizeRange: [90, 300],
+  platformHeightRange: [-5, 5],
+  platformHeightScale: 10,
+  platformVerticalLimit: [0.4, 0.7],
+  playerGravity: 900,
+  jumpForce: 400,
+  playerStartPosition: 200,
+  jumps: 2,
+  coinPercent: 25,
+};
