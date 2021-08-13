9813dc205f1f257065099982987ae85880b2c4cb
samroodAli <devsamrood@gmail.com>
2021-08-13 14:13:26 +0530

    Scores:Create a scene for leaderboard with createText function

---
+++ src/scenes/scores.js
@@ -0,0 +1,16 @@
+import Background from "./background";
+
+class Scores extends Background {
+  createText(string, posX, posY, fontSize, fill) {
+    const text = this.add.text(
+      posX ? posX : gameConfig.width / 2.5,
+      posY ? posY : gameConfig.height / 4,
+      string,
+      {
+        fill: fill ? fill : "#000",
+        fontSize: fontSize ? fontSize : "40px",
+      }
+    );
+    return text;
+  }
+}
