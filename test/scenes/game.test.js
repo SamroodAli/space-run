cba706679627f8bb94eb4a225897d5166b59e62a
samroodAli <devsamrood@gmail.com>
2021-08-16 23:00:42 +0530

    Test:Start testing the application

--- test/scenes/game.test.js
+++
@@ -1,5 +0,0 @@
-import LeaderBoard from '../../src/scenes/leaderBoard.js';
-
-test('check game is an instance of Game', () => {
-  const actual = new LeaderBoard();
-});
---
+++ test/scenes/leaderboard.test.js
@@ -0,0 +1,5 @@
+import LeaderBoard from "../../src/scenes/leaderBoard";
+
+const leaderBoard = new LeaderBoard();
+
+test("leader board initial getLeaderboard", () => {});
