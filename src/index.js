6a1d21d5fa7c1fba578897c8e6ac89ef228fd6ae
samroodAli <devsamrood@gmail.com>
2021-08-16 23:46:34 +0530

    Fix:Add content to display before the game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './style.scss';
-import Phaser from 'phaser';
-import { gameConfig } from './gameOptions.js';
-import Game from './scenes/game.js';
-import Main from './scenes/main.js';
-import PreloadGame from './scenes/preload.js';
+import "./style.scss";
+import Phaser from "phaser";
+import { gameConfig } from "./gameOptions.js";
+import Game from "./scenes/game.js";
+import Main from "./scenes/main.js";
+import PreloadGame from "./scenes/preload.js";
 
 gameConfig.scene = [PreloadGame, Main, Game];
 
