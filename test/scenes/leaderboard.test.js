3a9e4c83aa5e06d2b124eb2bf9c9fe1e86b7f5ed
samroodAli <devsamrood@gmail.com>
2021-08-18 18:26:44 +0530

    Test:Test class instance leaderboard

--- test/scenes/leaderboard.test.js
+++ test/scenes/leaderboard.test.js
@@ -1,10 +1,9 @@
-import LeaderBoard from "../../src/scenes/leaderBoard.js";
+import Leaderboard from "../../src/scenes/leaderBoard.js";
 import document from "../document.js";
 
-beforeAll(() => {
-  // const leaderBoard = new LeaderBoard();
-});
-
-test("leader board initial getLeaderboard", () => {
-  // expect(leaderboard).toBeInstanceOf(Leaderboard);
+describe("testing the leaderboard class methods", () => {
+  test("leaderboard construction is valid", () => {
+    const leaderboard = new Leaderboard();
+    expect(leaderboard).toBeDefined();
+  });
 });
